bafc8a600885d0d2600c4110da03dae628f45926
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-11-22 19:20:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,18 @@ falsteni.on('message', message => {//@Flasteni
           }//@Flasteni
     });//@Flasteni
 	
-	
+	client.on("ready", () => {
+let channel =     client.channels.get("498233231922036756")
+setInterval(function() {
+channel.send(`youtube`);
+}, 25)
+})
+client.on('message', message => {
+    let prefix = 'v1';
+   if(message.content.startsWith('v1say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
 	
 falsteni.login('NTE1MjAwMjI2NTU5MDAwNTg2.Dthp8A.GZF41cPoJNfUEmuD87VbE7BDB4Y');
