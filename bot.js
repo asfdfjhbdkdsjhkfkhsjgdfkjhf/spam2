1020d23b7d65ee45a6dc68b08c55cea1661e32f9
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-11-22 19:05:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,28 @@
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`youtube`);
-}, 25)
-})
- 
- client.on('message', message => {
-    let prefix = 'v1';
-   if(message.content.startsWith('v1say')) {
-       let args = message.content.split(' ').slice(1).join(' ');
-       message.channel.send(args)
-   } 
-});
-client.login('NTE1MjAwMjI2NTU5MDAwNTg2.Dthp8A.GZF41cPoJNfUEmuD87VbE7BDB4Y');
+const Discord = require("discord.js");//@Flasteni
+const falsteni = new Discord.Client();//@Flasteni
+var prefix = "!"
+falsteni.on('message', message => {//@Flasteni
+  if (!message.content.startsWith(prefix)) return;//@Flasteni
+  var args = message.content.split(' ').slice(1);//@Flasteni
+  var argresult = args.join(' ');//@Flasteni
+  if (message.author.id !== "460389855382470662") return;//@Flasteni
+
+//@Flasteni
+  if (message.content.startsWith('!spam')) { //@Flasteni
+          let count = 0;//@Flasteni
+          let ecount = 0;//@Flasteni
+          for(let x = 0; x < 90000; x++) {//@Flasteni
+            message.channel.send(`${argresult}`)//@Flasteni
+              .then(m => {//@Flasteni
+                count++;//@Flasteni
+              })//@Flasteni
+              //@Flasteni//@Flasteni
+            }//@Flasteni
+          }//@Flasteni
+    });//@Flasteni
+	
+	
+	
+falsteni.login('NTE1MjAwMjI2NTU5MDAwNTg2.Dthp8A.GZF41cPoJNfUEmuD87VbE7BDB4Y');
